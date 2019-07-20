4191b029a62d5123d75f5faf7a9a6f754554fd6c
macymuhia <macymuhia@gmail.com>
2019-07-20 16:35:40 +0300

    Add an init method

--- password.py
+++ password.py
@@ -2,6 +2,16 @@ class Credentials():
     """
     Class that generates new instances of passwords
     """
+    def __init__():
+        """
+        __init__ method that helps us define properties for our objects.
+        Args:
+        website: Website whose credentials need to be saved
+        user_name: username of the account
+        password: password of the account
+        """
+        pass
+
     pass
 
 
