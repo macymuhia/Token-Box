6aa5fecbce9cc3d07fdff011eaf5f28bc07e01d6
macymuhia <macymuhia@gmail.com>
2019-07-20 15:50:26 +0300

    Added an empty user class

--- password.py
+++ password.py
@@ -3,3 +3,10 @@ class Credentials():
     Class that generates new instances of passwords
     """
     pass
+
+
+class User():
+    """
+    Class that generates new instances of a user
+    """
+    pass
