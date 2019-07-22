17bc366e134c5cf9bea5a6b3e6afc8bf3674a477
macymuhia <macymuhia@gmail.com>
2019-07-22 08:25:54 +0300

    Add methods to:
    1.delete credentials
    2. find credentials by wesite url
    3. check if credentials exist using website url
    4. display all credentials
    5. copy password of found credentials

--- credentials.py
+++ credentials.py
@@ -1,3 +1,6 @@
+import pyperclip
+
+
 class Credentials:
     """
     Class that generates new instances of passwords
@@ -23,6 +26,59 @@ class Credentials:
         """
 
         Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        """
+        delete_credentials method deletes a saved contact from the contact list
+        :return:
+        """
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_website(cls, website_url):
+        """
+        method that takes in a website and returns credentials that match that website
+        Args:
+            website_url: website to search for
+        Returns:
+            credentials that match the website
+        """
+
+        for credential in cls.credentials_list:
+            if credential.website == website_url:
+                return credential
+
+    @classmethod
+    def credential_exists(cls, website_url):
+        """
+        Method checks if a credential exists from the credentials list
+        :param website_url: website_url to search if it exists
+        :return: Boolean: True or False depending on whether the credential exists
+        """
+        for credential in cls.credentials_list:
+            if credential.website == website_url:
+                return True
+        return False
+
+    @classmethod
+    def display_credentials(cls):
+        """
+        method that returns the credentials list
+        :return: List
+        """
+        return cls.credentials_list
+
+    @classmethod
+    def copy_password(cls, website_url):
+        credentials_found = Credentials.find_by_website(website_url)
+        pyperclip.copy(credentials_found.password)
+
+
+# cred1 = Credentials("google1.com", "macy1", "macy123")
+# cred2 = Credentials("google2.com", "macy2", "macy223")
+# cred3 = Credentials("google3.com", "macy3", "macy323")
+# cred4 = Credentials("google4.com", "macy4", "macy423")
+# cred5 = Credentials("google5.com", "macy5", "macy523")
 
 
 class User:
